9113f409aed997a9d22e1bbc8c4ef12170559f6f
damikik1 <61684196+damikik1@users.noreply.github.com>
2021-07-13 14:49:09 +0200

    Update create script
    Changed cipher_id in cipher_game to optional because datagrip is nazi mod. Also could pose problems in the long run.

--- database/create_script_shifra.sql
+++ database/create_script_shifra.sql
@@ -38,7 +38,7 @@ ALTER TABLE cipher ADD CONSTRAINT pk_cipher PRIMARY KEY (cipher_id);
 
 CREATE TABLE cipher_game (
     cipher_game_id SERIAL NOT NULL,
-    cipher_id INTEGER NOT NULL,
+    cipher_id INTEGER,
     name VARCHAR(256) NOT NULL,
     description VARCHAR(256) NOT NULL,
     visible_from TIMESTAMP NOT NULL,
