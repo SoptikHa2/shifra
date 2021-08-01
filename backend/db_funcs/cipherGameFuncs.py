e60cb3a6df5252ab6b1798f2e9a641e726f48183
bartoj35 <72392941+bartoj35@users.noreply.github.com>
2021-08-01 17:02:38 +0200

    Update cipherGameFuncs.py

--- backend/db_funcs/cipherGameFuncs.py
+++ backend/db_funcs/cipherGameFuncs.py
@@ -65,31 +65,6 @@ def getCipherGame(cipher_game_id: int):
         return {"result": "error"}
     return result
 
-
-def get_leaderboard(cipher_game_id: int):
-    if not is_visible(cipher_game_id):
-        return {"result": "game is not visible"}
-    try:
-        with DB_conn.getConn(connection):
-            with DB_conn.getCursor(connection) as cur:
-                cur.execute("SELECT t.team_id, t.name, SUM(c.score) FROM team t JOIN attempt a ON a.team_id = t.team_id AND a.is_successful = TRUE JOIN cipher c ON c.cipher_id = a.cipher_id AND c.cipher_game_id = %s GROUP BY t.team_id, t.name, c.score;", cipher_game_id)
-                result = cur.fetchall()
-    except:
-        return None
-    return result
-
-
-def is_visible(cipher_game_id: int):
-    timestamp = datetime.now()
-    try:
-        with DB_conn.getConn(connection):
-            with DB_conn.getCursor(connection) as cur:
-                cur.execute("SELECT * FROM cipher_game WHERE visible_from >=  %s AND cipher_game_id = %s;", (timestamp,cipher_game_id))
-    except:
-        return False
-    return True
-
-
 def get_ciphers(cipher_game_id: int):
     if # TODO: is root condition
         try:
@@ -110,13 +85,3 @@ def get_ciphers(cipher_game_id: int):
         except:
             return None
         return result
-
-def get_cipher_game(cipher_game_id: int):
-    try:
-        with DB_conn.getConn(connection):
-            with DB_conn.getCursor(connection) as cur:
-                cur.execute("SELECT * FROM cipher_game WHERE cipher_game_id = %s;", (cipher_game_id,))
-                result = cur.fetchall()
-    except:
-        return None 
-    return result
