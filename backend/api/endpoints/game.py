f9f497ad925d55d6727ca2b0ea8fa3c33ca20123
bartoj35 <72392941+bartoj35@users.noreply.github.com>
2021-08-01 17:01:02 +0200

    Update game.py

--- backend/api/endpoints/game.py
+++ backend/api/endpoints/game.py
@@ -5,13 +5,6 @@ from db_funcs import cipherGameFuncs
 
 router = APIRouter()
 
-@router.get('/api/leaderboard/{cipher_game_id}')
-def get_leaderboard(cipher_game_id: int):
-    leaderboard = cipherGameFuncs.get_leaderboard(cipher_game_id)
-    if leaderboard is None:
-        return {"result": "error occured"}
-    return leaderboard
-
 
 @router.get('/api/ciphers/{cipher_game_id}')
 def get_ciphers(cipher_game_id: int):
@@ -19,11 +12,3 @@ def get_ciphers(cipher_game_id: int):
     if ciphers is None:
         return {"error": "occured"}
     return ciphers
-
-
-@router.get("/api/game/{cipher_game_id}")
-def get_cipher_game(cipher_game_id: int):
-    if cipherGameFuncs.get_cipher_game(cipher_game_id)
-        return {"result": "error"}
-    return result
-
