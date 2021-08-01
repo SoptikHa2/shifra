b0a052f9eae4e7bc8e0f53384cc00296216a61ab
Petr Šťastný <petr@soptik.tech>
2021-08-01 11:51:44 +0200

    Added person-related queries to DB

--- backend/routes/person.py
+++ backend/routes/person.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Any
 
 
 class Person(BaseModel):
     person_id: Optional[int] = None
-    is_root: bool
+    is_root: bool = False
     nickname: str
     session_cookie: Optional[str] = None
     mail: Optional[str] = None
     password: Optional[str] = None
 
 
+def person_from_db_row(row) -> Person:
+    return Person(person_id=row[0], is_roow=row[1], nickname=row[2], session_cookie=row[3], mail=row[4], password=row[5])
