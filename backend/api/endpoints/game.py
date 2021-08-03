5c22e8aff00558eb1bcf48e48fe528bd11e648b0
bartoj35 <72392941+bartoj35@users.noreply.github.com>
2021-08-03 09:05:55 +0200

    Add http status codes

--- backend/api/endpoints/game.py
+++ backend/api/endpoints/game.py
@@ -6,9 +6,11 @@ from db_funcs import cipherGameFuncs
 router = APIRouter()
 
 @router.get('/api/leaderboard/{cipher_game_id}')
-def get_leaderboard(cipher_game_id: int):
+def get_leaderboard(cipher_game_id: int, response: Response):
     leaderboard = cipherGameFuncs.get_leaderboard(cipher_game_id)
     if leaderboard is None:
-        return {"result": "error occured"}
+        response.status_code = 400
+    else:
+        response.status_code = 200
     return leaderboard
 
