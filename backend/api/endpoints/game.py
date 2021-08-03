bb572f02aa5fc33ff5c0c4b1656a9e6b219e9aa7
bartoj35 <72392941+bartoj35@users.noreply.github.com>
2021-08-03 09:07:47 +0200

    Add http status codes

--- backend/api/endpoints/game.py
+++ backend/api/endpoints/game.py
@@ -8,13 +8,15 @@ from routes import Person
 router = APIRouter()
 
 @router.get("/api/games")
-def get_all_games(session_cookie: Optional[str] = Cookie(None)):
+def get_all_games(session_cookie: Optional[str] = Cookie(None), response: Response):
     user = user_management.get_user_by_token(session_cookie)
     if user.is_root:
         games = get_games()
     else:
         games = get_all_games(user.person_id)
     if games is None:
-        return {"result": "error occured"}
+        response.status_code = 400
+    else:
+        response.status_code = 200
     return games
 
