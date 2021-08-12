6bdcba86f9d38372dd8485d81d5ba408355271dd
Pavel Baštinec <bastipav@fit.cvut.cz>
2021-08-13 01:27:05 +0200

    cipher-game list

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from "@angular/common/http";
+import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/cipher-games/cipher-games.module.ts
+++ frontend/src/app/cipher-games/cipher-games.module.ts
@@ -3,6 +3,10 @@ import { CommonModule } from '@angular/common';
 import { CipherGameListComponent } from './cipher-game-list/cipher-game-list.component';
 import {RouterModule, Routes} from "@angular/router";
 import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatRippleModule} from "@angular/material/core";
+import {MatDatepickerModule} from "@angular/material/datepicker";
 
 const routes: Routes = [
   {path: '', component: CipherGameListComponent}
@@ -15,7 +19,11 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    MatCardModule
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatRippleModule,
+    MatDatepickerModule
   ]
 })
 export class CipherGamesModule { }
