d9da9c84c9c368d63dcb690f0388403c546daec9
bartoj35 <72392941+bartoj35@users.noreply.github.com>
2021-08-13 09:58:30 +0200

    Add comments

--- backend/api/endpoints/game.py
+++ backend/api/endpoints/game.py
@@ -9,10 +9,16 @@ router = APIRouter()
 
 @router.get('/api/ciphers/{cipher_game_id}')
 def get_visible_ciphers(cipher_game_id: int, response: Response, session_cookie: Optional[str] = Cookie(None)):
+    """
+    Show all visible ciphers from specified game
+    :param cipher_game_id: Game id
+    :return: 200 if everything was ok, 400 if no visible ciphers, 401 for not authenticated user
+             list of visible ciphers with striped informations
+    """
     user = user_management.get_user_by_token(session_cookie)
     if user is None:
         response.status_code = 401
-        return {"result": "user is not authorized"}
+        return None
     
     if cipherGameFuncs.is_game_organizer(cipher_game_id, user.person_id) or user.is_root:
         visible_ciphers = cipherGameFuncs.get_all_ciphers(cipher_game_id)
@@ -21,7 +27,7 @@ def get_visible_ciphers(cipher_game_id: int, response: Response, session_cookie:
     
     if visible_ciphers is None:
         response.status_code = 400
-        return {"result": "error occured"}
+        return None
     response.status_code = 200
     result = [x.strip() for x in visible_ciphers]
     return result
