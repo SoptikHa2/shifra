a8f314793b598edb32b992533a7725a20e68cb45
Pavel Baštinec <bastipav@fit.cvut.cz>
2021-08-14 10:41:38 +0200

    move '/games' route to '/'

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'games', loadChildren: () => import('./cipher-games/cipher-games.module').then(m => m.CipherGamesModule)},
+  {path: '', loadChildren: () => import('./cipher-games/cipher-games.module').then(m => m.CipherGamesModule)},
   {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
