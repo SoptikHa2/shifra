34511acf09bd1e441eb203a24e152fab39d1bf90
Pavel Baštinec <pavel4628@email.cz>
2021-08-18 12:40:32 +0200

    add team route to main route module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'team',
+    loadChildren: () => import('./team/team.module').then(m => m.TeamModule)
   }
 ];
 
