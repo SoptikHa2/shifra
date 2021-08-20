e668c9272ea52e1f83530f3fa03762ab68d5384d
Pavel Baštinec <pavel4628@email.cz>
2021-08-20 21:09:36 +0200

    add create team component

--- frontend/src/app/team/create-team/create-team.component.ts
+++ frontend/src/app/team/create-team/create-team.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {TeamService} from "../team.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-team',
   templateUrl: './create-team.component.html',
-  styleUrls: ['./create-team.component.scss']
+  styleUrls: ['./create-team.component.scss', '../../theme/form-theme.scss']
 })
 export class CreateTeamComponent implements OnInit {
+  createTeamForm: FormGroup;
+  teamName: FormControl;
 
-  constructor() { }
+  constructor(private teamService: TeamService) {
+    this.teamName = new FormControl('',
+      [Validators.required, Validators.minLength(4), Validators.maxLength(80)]);
+
+    this.createTeamForm = new FormGroup({
+      name: this.teamName
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  createTeam() {
+    this.teamService.createTeam(this.teamName.value);
+  }
 }
