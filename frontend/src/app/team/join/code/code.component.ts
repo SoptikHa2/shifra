a59d2fc828e19ef607c20964d2574e6ba03bf27d
Pavel Baštinec <pavel4628@email.cz>
2021-08-25 18:13:36 +0200

    join using code form

--- frontend/src/app/team/join/code/code.component.ts
+++ frontend/src/app/team/join/code/code.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {TeamService} from "../../team.service";
 
 @Component({
   selector: 'app-code',
   templateUrl: './code.component.html',
-  styleUrls: ['./code.component.scss']
+  styleUrls: ['./code.component.scss', '../../../theme/form-theme.scss']
 })
 export class CodeComponent implements OnInit {
+  joinTeamForm: FormGroup;
+  codeControl: FormControl;
 
-  constructor() { }
+  constructor(
+    private teamService: TeamService
+  ) {
+    this.codeControl = new FormControl('', [Validators.required]);
+    this.joinTeamForm = new FormGroup({
+      code: this.codeControl
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  joinTeam() {
+    //this.teamService.
+  }
 }
--- frontend/src/app/team/team.service.ts
+++ frontend/src/app/team/team.service.ts
@@ -57,4 +57,8 @@ export class TeamService {
       `${environment.backendUrl}/api/generateTeamJoinQR/${code}`);
     return this.sanitizer.bypassSecurityTrustUrl('https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FQR_code&psig=AOvVaw0XKpRON6z_3IcFBhHXxTXh&ust=1629620256736000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPino9vWwfICFQAAAAAdAAAAABAD');
   }
+
+  joinTeam(code: string) {
+    //this.http.post(`/api/`)
+  }
 }
