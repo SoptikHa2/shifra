0d773c828fa9dbc4adae6b92ab5e4debc8045432
Pavel Baštinec <pavel4628@email.cz>
2021-08-26 00:20:41 +0200

    update join using code

--- frontend/src/app/team/join/code/code.component.ts
+++ frontend/src/app/team/join/code/code.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {TeamService} from "../../team.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-code',
@@ -12,7 +13,8 @@ export class CodeComponent implements OnInit {
   codeControl: FormControl;
 
   constructor(
-    private teamService: TeamService
+    private teamService: TeamService,
+    private router: Router
   ) {
     this.codeControl = new FormControl('', [Validators.required]);
     this.joinTeamForm = new FormGroup({
@@ -24,6 +26,13 @@ export class CodeComponent implements OnInit {
   }
 
   joinTeam() {
-    //this.teamService.
+    this.teamService.joinTeam(this.codeControl.value)
+      .then(success => {
+        if (success) {
+          this.router.navigate(['team']).then();
+        } else {
+
+        }
+      });
   }
 }
--- frontend/src/app/team/team.service.ts
+++ frontend/src/app/team/team.service.ts
@@ -58,7 +58,14 @@ export class TeamService {
     return this.sanitizer.bypassSecurityTrustUrl('https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FQR_code&psig=AOvVaw0XKpRON6z_3IcFBhHXxTXh&ust=1629620256736000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPino9vWwfICFQAAAAAdAAAAABAD');
   }
 
-  joinTeam(code: string) {
-    //this.http.post(`/api/`)
+  joinTeam(code: string): Promise<boolean> {
+    return this.http.post(`${environment.backendUrl}/api/team/join`, {})
+      .pipe(
+        map(() => true),
+        catchError(err => {
+          if (!environment.production) console.error(err);
+          return of (false);
+        })
+      ).toPromise();
   }
 }
