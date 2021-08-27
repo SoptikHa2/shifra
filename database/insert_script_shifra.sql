931edca0de405d54326a8ec825423a82a717bb97
Petr Šťastný <petr@soptik.tech>
2021-08-27 18:56:02 +0200

    Minor insert script changes

--- database/insert_script_shifra.sql
+++ database/insert_script_shifra.sql
@@ -1,3 +1,6 @@
+
+BEGIN TRANSACTION;
+
 TRUNCATE TABLE attempt, cipher, hint, person, cipher_game, team, hint_used, cipher_game_admin, cipher_game_team, team_member CASCADE;
 
 ALTER SEQUENCE cipher_cipher_id_seq RESTART WITH 1;
@@ -10,31 +13,31 @@ ALTER SEQUENCE team_team_id_seq RESTART WITH 1;
 ------------------------------------------------------------------------------------------------------------------------
 INSERT INTO cipher_game (time_starting_cipher_id, name, description, visible_from, deadline_signup, deadline_event, capacity, teammax, password, autoapprove)
 VALUES (null, 'CipherGame1', 'First cipher', '2021-10-10 16:30', '2021-10-20 23:59', '2021-10-22 23:59', 200, 4,
- 'b''\\yuV~L_\xdd\xce\xe7\xc7\xd7+|\x036EWKC\xb1\xba&\n\x1a\xae\x17x\x86P\xbeA''', true);
+ 'code1', true);
 
 INSERT INTO cipher_game (time_starting_cipher_id, name, description, visible_from, deadline_signup, deadline_event, capacity, teammax, password, autoapprove)
 VALUES (null, 'CipherGame2', 'Cipher 2', '2021-12-10 00:00', '2021-12-12 23:59', '2021-12-31 23:59', 500, 2,
- 'b''\x93\xda8\xb4\x99\x11l\x05\xd0\xbf\x82\x93\xd2\xe8\xfd\xc4aK\xc5S]\x1f\x90\x98~\x15\xa5\x84]Wy\xbb''', false);
+ 'code2', false);
 
 INSERT INTO cipher_game (time_starting_cipher_id, name, description, visible_from, deadline_signup, deadline_event, capacity, teammax, password, autoapprove)
 VALUES (null, 'CipherGame3', 'Cipher 3', '2021-10-10 16:30', '2021-10-20 23:59', '2021-10-22 23:59', 300, 4,
- 'b''\\yuV~L_\xdd\xce\xe7\xc7\xd7+|\x036EWKC\xb1\xba&\n\x1a\xae\x17x\x86P\xbeA''', true);
+ 'code3', true);
 
 INSERT INTO cipher_game (time_starting_cipher_id, name, description, visible_from, deadline_signup, deadline_event, capacity, teammax, password, autoapprove)
 VALUES (null, 'CipherGame4', 'Cipher 4', '2021-10-10 16:30', '2021-10-20 23:59', '2021-10-22 23:59', 400, 4,
- 'b"\xa7M\xd1U\x91N\xb5\xc8\xffmR''m\x9a\x08\xff\xd3\xfc\xb9\xb5\xba\xd3\xc8\xc6\xbc\xd8^\xef\xab\xb7^J"', false);
+ 'code4', false);
 
 INSERT INTO cipher_game (time_starting_cipher_id, name, description, visible_from, deadline_signup, deadline_event, capacity, teammax, password, autoapprove)
 VALUES (null, 'CipherGame5', 'Cipher 5', '2021-10-10 16:30', '2021-10-20 23:59', '2021-10-22 23:59', 100, 4,
-'b''O\x85\xe3L)\xe4\xe3qa\xd1\x95\x87\xd3O\x90\xad\x83\x15\x9b\x06}W\x92\xf6\xd2X1\n\x1a\xc9\xfb&''', false);
+'code5', false);
 
 INSERT INTO cipher_game (time_starting_cipher_id, name, description, visible_from, deadline_signup, deadline_event, capacity, teammax, password, autoapprove)
 VALUES (null, 'Dejvicka sifrovacka', 'What do you expect me to say', '2021-10-10 16:30', '2021-10-20 23:59', '2021-10-22 23:59', 200, 4,
- 'b''\x93\xda8\xb4\x99\x11l\x05\xd0\xbf\x82\x93\xd2\xe8\xfd\xc4aK\xc5S]\x1f\x90\x98~\x15\xa5\x84]Wy\xbb''', true);
+ 'code6', true);
 
 INSERT INTO cipher_game (time_starting_cipher_id, name, description, visible_from, deadline_signup, deadline_event, capacity, teammax, password, autoapprove)
 VALUES (null, 'FIT', 'Biggest cipher game of them all', '2020-9-21 00:00', '2021-04-20 23:59', '2023-06-30 23:59', 800, 1,
- 'b''O\x85\xe3L)\xe4\xe3qa\xd1\x95\x87\xd3O\x90\xad\x83\x15\x9b\x06}W\x92\xf6\xd2X1\n\x1a\xc9\xfb&''', false);
+ 'code7', false);
 ------------------------------------------------------------------------------------------------------------------------
 
 -- Cipher data
@@ -103,31 +106,22 @@ INSERT INTO hint (cipher_id, msg, img, hint_file, score_cost, time_cost)
 VALUES (10, 'xd', 'img.jpg', 'file.txt', 200, 300 );
 ------------------------------------------------------------------------------------------------------------------------
 
--- Person data
+-- Person data (password = nickname)
 ------------------------------------------------------------------------------------------------------------------------
 INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (true, 'Soptik', null, null, 'b''\x93\xda8\xb4\x99\x11l\x05\xd0\xbf\x82\x93\xd2\xe8\xfd\xc4aK\xc5S]\x1f\x90\x98~\x15\xa5\x84]Wy\xbb''');
+VALUES (true, 'soptik', null, 'petr@soptik.tech', 'b''\x8dHj;]da\xc4\xcc\xf6\xe2\x90s\x1a-,}4>)\x94\x0eP\x91\x08\x10mc\x83:5\xb5''');
 
 INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (true, 'Marek', null, null, 'b''\x93\xda8\xb4\x99\x11l\x05\xd0\xbf\x82\x93\xd2\xe8\xfd\xc4aK\xc5S]\x1f\x90\x98~\x15\xa5\x84]Wy\xbb''');
+VALUES (true, 'marek', null, 'marek@example.org', 'b''\xa7\xda;w=|\x8e\x19\x88e\xe8\x95\xddm\xe8\x13\xff\xbd\xe1\x88@:\x85\xeaK\x05\xd7\xc4\x10\xd3\xb7]''');
 
 INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (true, 'Damian', null, null, 'b''\\yuV~L_\xdd\xce\xe7\xc7\xd7+|\x036EWKC\xb1\xba&\n\x1a\xae\x17x\x86P\xbeA''');
+VALUES (true, 'damian', null, 'damian@example.org', 'b''\xf27\xeet~<\xf1\xa2\xd4eX;\xf3\xa7\xc2\xbbI\xd6\xcddy\nht\xc8c\x18\xc0\x1d\xa1\x1b\xec''');
 
 INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (false, 'Bot1', null, null, 'b"\xa7M\xd1U\x91N\xb5\xc8\xffmR''m\x9a\x08\xff\xd3\xfc\xb9\xb5\xba\xd3\xc8\xc6\xbc\xd8^\xef\xab\xb7^J"');
+VALUES (false, 'bot1', null, 'bot@example.org', 'b"&=\x87\xbe[\x16\x80pJ\x08\x93\xd4\x9a\x884\xd6\x17\xafs\x9b\xc0\xc3j\x92\xf5\xaa\xe3\xc4\xce\x1b5\x9a"');
 
 INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (false, 'Bot2', null, null, 'b''O\x85\xe3L)\xe4\xe3qa\xd1\x95\x87\xd3O\x90\xad\x83\x15\x9b\x06}W\x92\xf6\xd2X1\n\x1a\xc9\xfb&''');
-
-INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (false, 'Bot3', null, null, 'b''\x93\xda8\xb4\x99\x11l\x05\xd0\xbf\x82\x93\xd2\xe8\xfd\xc4aK\xc5S]\x1f\x90\x98~\x15\xa5\x84]Wy\xbb''');
-
-INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (false, 'Bot4', null, null, 'b''O\x85\xe3L)\xe4\xe3qa\xd1\x95\x87\xd3O\x90\xad\x83\x15\x9b\x06}W\x92\xf6\xd2X1\n\x1a\xc9\xfb&''');
-
-INSERT INTO person (is_root, nickname, session_cookie, mail, password)
-VALUES (false, 'Bot5', null, null, 'b''O\x85\xe3L)\xe4\xe3qa\xd1\x95\x87\xd3O\x90\xad\x83\x15\x9b\x06}W\x92\xf6\xd2X1\n\x1a\xc9\xfb&''');
+VALUES (false, 'bot2', null, null, 'b''%\x1aX\xfc\x8e\xe2\xfcH\x00\xa6\x19\xc8\xc0\xc4Hs\xf9\x91\xcdh\xb6P\\\xb7X\xdc\xfc \xa8\xd2\\\x96''');
 ------------------------------------------------------------------------------------------------------------------------
 
 -- Team data
@@ -142,11 +136,11 @@ INSERT INTO team (name, invite_code, approved) VALUES ('Bots2', '', false);
 INSERT INTO team_member (person_id, team_id) VALUES (1,1);
 INSERT INTO team_member (person_id, team_id) VALUES (2,1);
 INSERT INTO team_member (person_id, team_id) VALUES (3,1);
+
 INSERT INTO team_member (person_id, team_id) VALUES (4,2);
-INSERT INTO team_member (person_id, team_id) VALUES (5,2);
-INSERT INTO team_member (person_id, team_id) VALUES (6,3);
-INSERT INTO team_member (person_id, team_id) VALUES (7,3);
-INSERT INTO team_member (person_id, team_id) VALUES (8,3);
+
+INSERT INTO team_member (person_id, team_id) VALUES (4,3);
+INSERT INTO team_member (person_id, team_id) VALUES (5,3);
 ------------------------------------------------------------------------------------------------------------------------
 
 -- Cipher_game_team data
@@ -156,30 +150,24 @@ INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (2,1);
 INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (3,1);
 INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (4,1);
 INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (5,1);
-INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (6,1);
-INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (7,1);
 
 INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (1,2);
 INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (2,2);
-INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (3,2);
+INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (6,2);
 
-INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (1,3);
 INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (4,3);
-INSERT INTO cipher_game_team (cipher_game_id, team_id) VALUES (7,3);
 ------------------------------------------------------------------------------------------------------------------------
 
 -- Cipher_game_person data
 ------------------------------------------------------------------------------------------------------------------------
 INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (1,1);
-INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (2,1);
-INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (3,1);
-INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (4,1);
-INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (5,1);
-INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (6,1);
-INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (7,1);
+INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (2,4);
+INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (3,4);
+INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (4,4);
+INSERT INTO cipher_game_admin (cipher_game_id, person_id) VALUES (5,5);
 ------------------------------------------------------------------------------------------------------------------------
 
--- Attempt data 
+-- Attempt data
 ------------------------------------------------------------------------------------------------------------------------
 INSERT INTO attempt (cipher_id, team_id, start_time, last_attempt_time, attempt_count, was_success) VALUES (1, 1, '2021-10-10 17:00', '2021-10-10 17:00', 1, true);
 INSERT INTO attempt (cipher_id, team_id, start_time, last_attempt_time, attempt_count, was_success) VALUES (2, 1, '2021-10-10 17:03', '2021-10-10 17:20', 4, true);
@@ -217,3 +205,5 @@ INSERT INTO hint_used (hint_id, team_id) VALUES (8,1);
 INSERT INTO hint_used (hint_id, team_id) VALUES (8,2);
 INSERT INTO hint_used (hint_id, team_id) VALUES (10,3);
 ------------------------------------------------------------------------------------------------------------------------
+
+COMMIT;
