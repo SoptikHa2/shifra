49af78e7bc43b29dde64b0cae3db607551a1d4ff
Petr Šťastný <petr@soptik.tech>
2021-08-27 18:32:46 +0200

    Made last attempt time optional in model

--- backend/routes/attempt.py
+++ backend/routes/attempt.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 from datetime import datetime
 
@@ -6,6 +8,6 @@ class Attempt(BaseModel):
     cipher_id: int
     team_id: int
     start_time: datetime
-    last_attempt_time: datetime
+    last_attempt_time: Optional[datetime]
     attempt_count: int
     was_success: bool
