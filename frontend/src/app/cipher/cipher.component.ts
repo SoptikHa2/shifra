b1910f704b34f432f8c07afbbcb672c1447a415e
Pavel Baštinec <pavel4628@email.cz>
2021-09-01 23:41:56 +0200

    update cipher view

--- frontend/src/app/cipher/cipher.component.ts
+++ frontend/src/app/cipher/cipher.component.ts
@@ -7,7 +7,7 @@ import {Hint} from "../model/hint";
 import {DomSanitizer} from "@angular/platform-browser";
 import {MatDialog} from "@angular/material/dialog";
 import {AskDialogComponent} from "../dialogs/ask-dialog/ask-dialog.component";
-import {skipWhile, switchMap} from "rxjs/operators";
+import {skipWhile, switchMap, tap} from "rxjs/operators";
 import {HintDialogComponent} from "./hint-dialog/hint-dialog.component";
 
 @Component({
@@ -17,6 +17,8 @@ import {HintDialogComponent} from "./hint-dialog/hint-dialog.component";
 })
 export class CipherComponent implements OnInit {
   cipherObs: Observable<Cipher>;
+  cipher: Cipher | undefined;
+  cipherId: number;
 
   constructor(
     private cipherService: CipherService,
@@ -24,8 +26,11 @@ export class CipherComponent implements OnInit {
     private dialog: MatDialog,
     private route: ActivatedRoute
   ) {
-    this.cipherObs = this.cipherService
-      .getCipher(this.route.snapshot.params['id'])
+    this.cipherId = this.route.snapshot.params['id'];
+    this.cipherObs = this.cipherService.getCipher(this.cipherId);
+    this.cipherObs.subscribe(cipher => {
+      this.cipher = cipher;
+    })
   }
 
   getHintCostText(hint: Hint) {
@@ -41,14 +46,34 @@ export class CipherComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  hintClick(hint_id: number) {
-    this.dialog.open(AskDialogComponent, {data: {text: 'Opravdu chcete nápovědu?'}}).afterClosed()
+  hintClick(hint_id: number, cipher: Cipher) {
+    const stripedHint = cipher.hints.find(h => h.hint_id === hint_id);
+
+    // If error occurs
+    if (stripedHint == null)
+      return;
+
+    if (stripedHint.score_cost == null && stripedHint.time_cost) {
+      this.cipherService.openHint(hint_id)
+        .pipe(
+          switchMap(hint => this.dialog.open(HintDialogComponent,
+            {data: hint, width: '100%'}).afterClosed())
+        ).subscribe();
+      return;
+    }
+
+    this.dialog.open(AskDialogComponent, {data: {text: 'Opravdu chcete nápovědu?'}})
+      .afterClosed()
       .pipe(
         skipWhile(res => !res),
         switchMap(res => {
           if (res)
             return this.cipherService.openHint(hint_id);
           return of(null);
+        }),
+        tap(hint => {
+          if (hint != null)
+            stripedHint.score_cost = stripedHint.time_cost = undefined;
         }),
         switchMap(hint => {
           return this.dialog.open(HintDialogComponent,
@@ -59,32 +84,8 @@ export class CipherComponent implements OnInit {
   trustUrl(url: string) {
     return this.domSanitizer.bypassSecurityTrustUrl(url);
   }
+
+  makeAttempt() {
+
+  }
 }
-/*
-# Lorem ipsum dolor
----
-sit amet, consectetuer **adipiscing elit**. **~~Ut tempus~~** purus at lorem. Vivamus porttitor turpis ac leo. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Curabitur sagittis hendrerit ante. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Nullam lectus justo, vulputate eget mollis sed, tempor sed magna. Nulla non lectus sed nisl molestie malesuada. Suspendisse nisl. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Etiam bibendum elit eget erat. Praesent dapibus.
-
->  *et magnis dis parturient montes nascetur ridiculus mus. Nullam eget nisl.*
->  ***Nulla quis diam**. Class aptent taciti sociosqu ad*
-
-# Aliquam ante.
----
-Praesent dapibus. Proin in tellus sit amet **nibh dignissim** sagittis. Nunc auctor. Integer vulputate sem a nibh rutrum consequat. Fusce wisi. Curabitur vitae diam non enim vestibulum interdum. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Aliquam ante. Fusce consectetuer risus a nunc. Proin in tellus sit amet nibh dignissim sagittis. Duis viverra diam non justo.
-
-
-# Morbi leo mi,
----
-nonummy eget tristique non, rhoncus non leo. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Nullam rhoncus aliquam metus. Etiam commodo dui eget wisi. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Proin in tellus sit amet nibh dignissim sagittis. In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Sed convallis magna eu sem. Quisque porta. Nullam rhoncus aliquam metus. Praesent dapibus.
-
-
-# Nullam at arcu
----
-a est sollicitudin euismod. Mauris dolor felis, **sagittis at**, luctus sed, aliquam non, tellus. Et harum quidem rerum facilis est et expedita distinctio. Suspendisse nisl. Fusce nibh. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris elementum mauris vitae tortor. Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui. Sed convallis magna eu sem. Nulla quis diam.
-
-
-# Cum sociis natoque
----
-penatibus et magnis dis parturient montes nascetur ridiculus mus. Nullam eget nisl. Nulla quis diam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Etiam dictum tincidunt diam. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Praesent vitae arcu tempor neque lacinia pretium. Nullam sit amet magna in magna gravida vehicula. Quisque porta.
-
-* */
