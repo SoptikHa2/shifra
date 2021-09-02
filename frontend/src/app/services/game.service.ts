1cefa4315a3194a81e68baa400c50328aab7cfbf
Pavel Baštinec <pavel4628@email.cz>
2021-09-02 14:52:33 +0200

    add loading to game service

--- frontend/src/app/services/game.service.ts
+++ frontend/src/app/services/game.service.ts
@@ -5,22 +5,26 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {catchError} from "rxjs/operators";
 import {Team} from "../model/team";
+import {LoadingService} from "./loading.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private loadingService: LoadingService
+  ) { }
 
   getGames(): Observable<[Game, Team | null][]> {
-    return this.http.get<[Game, Team | null][]>(environment.backendUrl + '/api/games')
-      .pipe(catchError(this.handleError));
+    return this.loadingService.startLoading(this.http.get<[Game, Team | null][]>(environment.backendUrl + '/api/games')
+      .pipe(catchError(this.handleError)));
   }
 
   getGameById(id: number): Observable<Game> {
-    return this.http.get<Game>( `${environment.backendUrl}/api/game/${id}`)
-      .pipe(catchError(this.handleError));
+    return this.loadingService.startLoading(this.http.get<Game>( `${environment.backendUrl}/api/game/${id}`)
+      .pipe(catchError(this.handleError)));
   }
 
   handleError(err: any) {
