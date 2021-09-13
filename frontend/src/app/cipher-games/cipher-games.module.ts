7c06fd7c7f6b233220ab1fd8cc697fcd66d55f1a
Pavel Baštinec <pavel4628@email.cz>
2021-09-14 01:01:13 +0200

    add detail and detail button

--- frontend/src/app/cipher-games/cipher-games.module.ts
+++ frontend/src/app/cipher-games/cipher-games.module.ts
@@ -4,20 +4,27 @@ import {RouterModule, Routes} from "@angular/router";
 import { PickGameComponent } from './pick-game/pick-game.component';
 import {GameCardModule} from "../game-card/game-card.module";
 import {MatButtonModule} from "@angular/material/button";
+import { DetailComponent } from './detail/detail.component';
+import {MarkdownModule} from "ngx-markdown";
+import {AttributeModule} from "../attribute/attribute.module";
 
 const routes: Routes = [
-  {path: 'pick', component: PickGameComponent}
+  {path: 'pick', component: PickGameComponent},
+  {path: 'detail/:id', component: DetailComponent}
 ]
 
 @NgModule({
   declarations: [
     PickGameComponent,
+    DetailComponent,
   ],
-  imports: [
-    CommonModule,
-    RouterModule.forChild(routes),
-    GameCardModule,
-    MatButtonModule,
-  ]
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes),
+        GameCardModule,
+        MatButtonModule,
+        MarkdownModule,
+        AttributeModule,
+    ]
 })
 export class CipherGamesModule { }
