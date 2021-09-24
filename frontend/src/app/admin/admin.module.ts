9365ea36d804562d6c29fc399fe0ade3b43c9922
Pavel Baštinec <pavel4628@email.cz>
2021-09-25 00:03:55 +0200

    add admin module

---
+++ frontend/src/app/admin/admin.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminComponent } from './admin.component';
+import {RouterModule, Routes} from "@angular/router";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatButtonModule} from "@angular/material/button";
+import {MarkdownModule} from "ngx-markdown";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSelectModule} from "@angular/material/select";
+import {ReactiveFormsModule} from "@angular/forms";
+import {CipherService} from "../services/admin/cipher.service";
+import {GameService} from "../services/admin/game.service";
+import { AddGameComponent } from './add-game/add-game.component';
+
+const routes: Routes = [
+  {
+    path: 'add-game',
+    component: AddGameComponent
+  },
+  {
+    path: '',
+    component: AdminComponent
+  },
+];
+
+@NgModule({
+  declarations: [
+    AdminComponent,
+    AddGameComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatButtonModule,
+    MarkdownModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+  ],
+  providers: [
+    CipherService,
+    GameService
+  ]
+})
+export class AdminModule { }
