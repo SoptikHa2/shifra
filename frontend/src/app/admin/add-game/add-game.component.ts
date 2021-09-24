e8a81b30b8bf5b93021cfb459cfaaf6e1569bf16
Pavel Baštinec <pavel4628@email.cz>
2021-09-25 00:03:24 +0200

    add game form

---
+++ frontend/src/app/admin/add-game/add-game.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {GameService} from "../../services/admin/game.service";
+
+@Component({
+  selector: 'app-add-game',
+  templateUrl: './add-game.component.html',
+  styleUrls: ['./add-game.component.scss']
+})
+export class AddGameComponent implements OnInit {
+
+  gameFormControl: FormGroup;
+
+  constructor(
+    private gameService: GameService
+  ) {
+    this.gameFormControl = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      visible_from: new FormControl('', [Validators.required]),
+      starts_at: new FormControl('', [Validators.required]),
+      deadline_signup: new FormControl('', [Validators.required]),
+      deadline_event: new FormControl('', [Validators.required]),
+      capacity: new FormControl('', [Validators.required]),
+      teammax: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      autoapprove: new FormControl(false, [Validators.required])
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  createGame() {
+    this.gameService.addGame({
+      ...this.gameFormControl.value,
+      cipher_id_to_start_timer: null,
+      image: ''
+    }).subscribe(() => {
+    }, err => {
+      alert('stala se neočekávaná chyba')
+      console.log(err);
+    });
+  }
+}
