aa7664e5d08dba09d2476ac95b43b0e780b39b45
Pavel Baštinec <pavel4628@email.cz>
2021-09-27 10:02:40 +0200

    update add hint view

--- frontend/src/app/admin/add-hint/add-hint.component.ts
+++ frontend/src/app/admin/add-hint/add-hint.component.ts
@@ -1,24 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import {HintService} from "../../services/admin/hint.service";
-import {FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {CipherService} from "../../services/cipher.service";
 
 @Component({
   selector: 'app-add-hint',
   templateUrl: './add-hint.component.html',
-  styleUrls: ['./add-hint.component.scss']
+  styleUrls: ['./add-hint.component.scss', '../admin-form-theme.scss']
 })
 export class AddHintComponent implements OnInit {
   hintFormGroup: FormGroup;
+  id: number | undefined;
 
   constructor(
-    private hintService: HintService
+    private hintService: HintService,
+    private cipherSerice: CipherService,
+    private route: ActivatedRoute
   ) {
+    this.id = this.route.snapshot.queryParams['edit'];
     this.hintFormGroup = new FormGroup({
-
+      msg: new FormControl('', [Validators.required]),
+      score_cost: new FormControl(null),
+      time_cost: new FormControl(null)
     });
+
+    if (this.id != null) {
+      this.cipherSerice.openHint(this.id).subscribe(val => {
+        this.hintFormGroup.setValue({msg: val.msg, score_cost: val.score_cost, time_cost: val.time_cost});
+      }, () => {
+        alert('chyba !!!');
+      });
+    }
   }
 
   ngOnInit(): void {
   }
 
+  submitted() {
+    if (this.id == undefined) {
+      this.hintService.
+    } else {
+
+    }
+  }
 }
