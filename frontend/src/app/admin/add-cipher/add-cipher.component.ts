bbf1b8f42eeee2d4119192b608ba7b4d6cb80875
Pavel Baštinec <pavel4628@email.cz>
2021-09-27 17:18:49 +0200

    update add-cipher

--- frontend/src/app/admin/add-cipher/add-cipher.component.ts
+++ frontend/src/app/admin/add-cipher/add-cipher.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
 import {GameService} from "../../services/admin/game.service";
 import {Observable} from "rxjs";
 import {Cipher} from "../../model/cipher";
 import {ActivatedRoute, Router} from "@angular/router";
-import {CipherService} from "../../services/admin/cipher.service";
+import {CipherService} from "../../services/cipher.service";
+import {CipherService as AdminCipherService} from "../../services/admin/cipher.service";
 
 @Component({
   selector: 'app-add-cipher',
@@ -14,10 +15,13 @@ import {CipherService} from "../../services/admin/cipher.service";
 export class AddCipherComponent implements OnInit {
   cipherFormGroup: FormGroup;
   gameCiphersObs: Observable<Cipher[]>
+  imageName: string | undefined;
+  cipher: Cipher | undefined;
 
   constructor(
     private gameService: GameService,
     private cipherService: CipherService,
+    private adminCipherService: AdminCipherService,
     private router: Router,
     private route: ActivatedRoute
   ) {
@@ -31,22 +35,64 @@ export class AddCipherComponent implements OnInit {
       cooldown: new FormControl('', [Validators.required]),
       attempts: new FormControl('', [Validators.required]),
       score: new FormControl('', [Validators.required]),
-      reference_solution: new FormControl('', [Validators.required])
+      reference_solution: new FormControl('', [Validators.required]),
+      img: new FormControl('', [Validators.required])
     });
 
     this.gameCiphersObs = this.gameService.getCiphers(this.route.snapshot.params['id']);
+
+    this.cipherService.getCipher(this.route.snapshot.queryParams['edit'])
+      .subscribe((cipher: Cipher) => {
+        this.cipherFormGroup.setValue({
+          name: cipher.name,
+          req_cipher_id: cipher.req_cipher_id,
+          description: cipher.description,
+          solution: cipher.solution,
+          judge: cipher.judge,
+          success_msg: cipher.success_msg,
+          cooldown: cipher.cooldown,
+          attempts: cipher.attempts,
+          score: cipher.score,
+          reference_solution: cipher.reference_solution,
+          img: cipher.img
+        });
+        this.cipher = cipher;
+      }, () => alert('chyba'));
   }
 
   ngOnInit(): void {
   }
 
-  createCipher() {
-    this.cipherService.addCipher(this.route.snapshot.params['id'], this.cipherFormGroup.value)
-      .subscribe(cipher => {
-        this.router.navigate(['/', 'admin', 'cipher', cipher.cipher_id]).then();
-      }, (err) => {
-        alert('nastala chyba');
-        console.error(err);
-      })
+  updateFile(event: any, control: AbstractControl) {
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        control.setValue(reader.result);
+      }
+    }
+  }
+
+  createOrUpdateCipher() {
+    if (!this.route.snapshot.queryParams['edit']) {
+      this.adminCipherService.addCipher(this.route.snapshot.params['id'], this.cipherFormGroup.value)
+        .subscribe(cipher => {
+          this.router.navigate(['/admin', 'cipher', cipher.cipher_id]).then();
+        }, (err) => {
+          alert('nastala chyba');
+          console.error(err);
+        })
+    } else {
+      this.adminCipherService.updateCipher({...this.cipher, ...this.cipherFormGroup.value})
+        .subscribe(cipher => {
+          this.router.navigate(['/admin', 'cipher', cipher.cipher_id]).then();
+        }, () => alert('chyba!!!'));
+    }
+  }
+
+  imageChanged(event: any) {
+    this.updateFile(event, this.cipherFormGroup.get('img')!);
+    this.imageName = event.target.files[0].name;
   }
 }
